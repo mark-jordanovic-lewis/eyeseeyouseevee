cd7a4e7c8ec2563a39a0af53c196efc752b89c46
Mark Jordanovic-Lewis <mark.4ndrew.lewis@gmail.com>
2019-03-22 01:40:41 +0000

    got proxy to sinatra working but still no stream from SPA site...

--- mock_server/lib/setup_app.rb
+++ mock_server/lib/setup_app.rb
@@ -1,10 +1,62 @@
 require 'sinatra/base'
-
+require 'watir'
+require 'headless'
 # =============== #
 # Single Page App # Should be admin app next dev phase
 # =============== #
 class SetupApp < Sinatra::Base
   get '/' do
-    'oi'
+    "This exist at least"
+    # uri = URI('http://127.0.0.1:4089')
+    # head = Net::HTTP.start(uri.host, uri.port) { |http| http.head(uri.request_uri) }
+    # headers 'Content-Type' => head['Content-Type']
+    # stream do |out|
+    #   Net::HTTP.get_response(uri) do |f|
+    #     f.read_body { |ch| out << ch }
+    #   end
+    # end
+
+
+    # nopes
+
+    # redirect 'http://127.0.0.1:4089/', 301
+
+    # require 'open_uri'
+    # open('http://127.0.0.1:4089/').read
+
+    # response = ""
+    # Rifler::headlessly do
+    #   gnome = Rifler::firefox
+    #   response = gnome.goto('view-source:http://127.0.0.1:4089/')
+    # end
+    # response
+  end
+end
+
+class Rifler
+  class << self
+    def firefox
+      Watir::Browser.new :firefox, profile: profile
+    end
+
+    def headlessly
+      headless = Headless.new
+      headless.start
+      yeild
+    ensure
+      headless.destroy
+    end
+
+    private
+
+    def profile
+      @profile ||= begin
+        profile = Selenium::WebDriver::Firefox::Profile.new
+        profile['browser.download.dir'] = './tmp/webdriver-downloads'
+        profile['browser.download.folderList'] = 2
+        profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/javascript'
+        profile
+      end
+    end
   end
 end
